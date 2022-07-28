68aee99847245c9493298fbe574459264b8929da
hyoredm <hyoredm@gmail.com>
2022-07-28 23:25:20 +0900

    commit

---
+++ Basic_26.js
@@ -0,0 +1,8 @@
+const superman = {
+    name : 'clark',
+    age : 30,
+    }
+    
+    console.log(superman.name) // "clark" 출력
+    console.log(superman['age']) // 30 출력
+    console.log(superman) // 위 객체 코드 그대로 출력
