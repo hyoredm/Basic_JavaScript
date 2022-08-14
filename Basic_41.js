524ac0b800d556ae38305f53ad355fd96f1aa399
hyoredm <hyoredm@gmail.com>
2022-08-14 23:43:54 +0900

    commit

---
+++ Basic_41.js
@@ -0,0 +1,7 @@
+let students = ['철수', '영희',..., '영수'];
+console.log(students[0]); // 철수
+console.log(students[1]); // 영희
+console.log(students[29]); // 영수
+
+students[0] = '민정'; // 철수 -> 민정으로 수정
+console.log(students) // ['민정', '영희',...
