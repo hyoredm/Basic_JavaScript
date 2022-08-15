5e3d878379fc77e8fdf1e5f59a913fe410d67993
hyoredm <hyoredm@gmail.com>
2022-08-15 20:21:56 +0900

    commit

---
+++ Basic_42.js
@@ -0,0 +1,12 @@
+let arr = [
+    '민수', // 문자
+    3, // 숫자
+    false, // Boolean
+     {
+    name: 'Mike',
+    age: 30, // 객체
+     },
+    function(){ // 함수
+    console.log('TEST');
+     }
+    ];
