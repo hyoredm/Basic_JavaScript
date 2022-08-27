75362db63ade6e4c32472f2c76e85c353c9b6f00
hyoredm <hyoredm@gmail.com>
2022-08-27 22:59:48 +0900

    commit

---
+++ dream_1.js
@@ -0,0 +1,7 @@
+<head>
+	<meta charset="utf-8"/>
+	<title>Document</title>
+	<script async src="a.js"></script>
+	<script async src="b.js"></script>
+	<script async src="c.js"></script>
+</head> // 다수의 스크립트를 다운 받을 때 다운로드 먼저 되는 것부터 실행
