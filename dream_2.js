947b45647120cdb39202022d222c5aead12db838
hyoredm <hyoredm@gmail.com>
2022-08-28 23:12:06 +0900

    commit

---
+++ dream_2.js
@@ -0,0 +1,7 @@
+<head>
+	<meta charset="utf-8"/>
+	<title>Document</title>
+	<script defer src="a.js"></script>
+	<script defer src="b.js"></script>
+	<script defer src="c.js"></script>
+</head> // parsing하는 동안 필요한 JavaScript 파일을 모두 다운 받은 후 순서대로 실행하기 때문에 원하는대로 실행 가능
